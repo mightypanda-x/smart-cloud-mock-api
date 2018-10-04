3aa0e5fb20c50d7f5ae15210b032147ee5d46c72
Nikhil Gupta <ngupta@ep.com>
2018-10-04 12:50:59 -0700

    CORS headers

--- routes/api.js
+++ routes/api.js
@@ -14,6 +14,10 @@ router.get('/dataPie', function(req, res) {
     value: 72
   }];
   setTimeout(() => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Request-Method', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+      res.setHeader('Access-Control-Allow-Headers', '*');
       res.send(jsonData);
   }, 200);
 });
