c990c135a8fbebca2b557079205a004f70a16924
Nikhil Gupta <ngupta@ep.com>
2018-10-04 12:20:00 -0700

    updated mock api

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/dataPie', function(req, res) {
+  const jsonData = [{
+    key: 'Red',
+    value: 50
+  }, {
+    key: 'Blue',
+    value: 32
+  }, {
+    key: 'Green',
+    value: 72
+  }];
+  setTimeout(() => {
+      res.send(jsonData);
+  }, 200);
 });
 
 module.exports = router;
