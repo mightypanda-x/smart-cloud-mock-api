f7fe430659c1adc556efb1727b106126604ed79a
Nikhil Gupta <ngupta@ep.com>
2018-10-25 14:49:28 -0700

    .

--- routes/api.js
+++ routes/api.js
@@ -48,4 +48,28 @@ router.get('/smart-start/union', function(req, res) {
   }, 200);
 });
 
+router.get('/smart-start/exception-crew', function(req, res) {
+    const jsonData = [{
+        key: 'Loanout',
+        value: _.random(50, 150, false)
+    }, {
+        key: 'Conflict Of Interest',
+        value: _.random(10, 25, false)
+    }, {
+        key: 'Allowances & Rentals',
+        value: _.random(1, 100, false)
+    }, {
+        key: 'I-9 Overdue',
+        value: _.random(25, 50, false)
+    }];
+
+    setTimeout(() => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Request-Method', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        res.send(jsonData);
+    }, 200);
+});
+
 module.exports = router;
